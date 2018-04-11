e346dfefccba570744557e12d2a37102fc4c4427
Rick Sun <yatyricky@gmail.com>
2018-04-11 17:19:29 +0800

    working with wc3 1.29

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @echo off
-node .\lni2json.js
-node .\json2w3a.js
-MPQEditor.exe add .\ZAM_ruins.w3x .\objects\war3map.w3a "war3map.w3a"
-echo Success - war3map.w3a injected to map
-REM clijasshelper.exe .\objects\common.j .\objects\blizzard.j .\ZAM_ruins.w3x
+REM node .\lni2json.js
+REM node .\json2w3a.js
+REM MPQEditor.exe add .\ZAM_ruins.w3x .\objects\war3map.w3a "war3map.w3a"
+REM echo Success - war3map.w3a injected to map
+clijasshelper.exe .\objects\common.j .\objects\blizzard.j .\src\Main.j .\ZAM_ruins.w3x
+"Warcraft III.exe" -window -loadfile "D:\Documents\Warcraft III\Maps\Zulaman-WE\ZAM_ruins.w3x"
