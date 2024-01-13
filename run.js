e562141ee799a83a2acc5005f58c8a8fda984f0a
Rick Sun <yatyricky@gmail.com>
2024-01-13 15:32:08 +0800

    fix launch problem when path contains whitespaces

--- run.js
+++ run.js
@@ -8,4 +8,4 @@ if (process.argv[2] === undefined) {
 
 const fpExec = path.join(config.wc3path, "_retail_/x86_64/Warcraft III.exe")
 
-child_process.execSync(`"${fpExec}" -launch -window -loadfile ${path.join(__dirname, "build/ZAM_ruins.w3x")}`)
+child_process.execSync(`"${fpExec}" -launch -window -loadfile "${path.join(__dirname, "build/ZAM_ruins.w3x")}"`)
